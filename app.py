648335a6f346e870312a57a99efc19ab5e7d43d0
ARNAUDVictor <142483758+ARNAUDVictor@users.noreply.github.com>
2025-11-11 10:37:19 +0100

    Refrecator: app.py simplification with blueprints

--- app.py
+++ app.py
@@ -1,58 +1,18 @@
-from flask import Flask, redirect, render_template, request, url_for
-from models import db, Task
+from flask import Flask
+from models import db
 from config import Config
+from blueprints.tasks.routes import tasks_blueprint
 
+#app creation
 app = Flask(__name__)
 
-###############Database Configuration####################
+# Load configuration from Config class
+app.config.from_object(Config)
 
 # Initialize the database
 db.init_app(app)
 
-
-##################Routes#######################
-
-# Home page - display tasks
-@app.route('/')
-def home():
-    task_list = Task.query.all()
-
-    return render_template('index.html', tasks=task_list)
-
-
-# Add a new task
-@app.route("/add_task", methods=['POST'])
-def add_task():
-    task = Task(title=request.form.get('title'))
-    db.session.add(task)
-    db.session.commit()
-
-    return redirect(url_for("home"))
-
-
-# Mark a task as complete
-@app.route("/mark_task_complete/<int:task_id>")
-def mark_task_complete(task_id):
-    task = Task.query.get(task_id)
-    if task:
-        task.completed = not task.completed
-        db.session.commit()
-
-    return redirect(url_for("home"))
-
-
-# Delete a task
-@app.route("/delete_task/<int:task_id>")
-def delete_task(task_id):
-    task = Task.query.get(task_id)
-    if task:
-        db.session.delete(task)
-        db.session.commit()
-
-    return redirect(url_for("home"))
-
-################Helper Functions####################
-
+app.register_blueprint(tasks_blueprint, url_prefix='/tasks')
 
 
 ########################Run the app########################
