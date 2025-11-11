4332c871a90564f9a03d7b8220ea1c926d04c14f
ARNAUDVictor <142483758+ARNAUDVictor@users.noreply.github.com>
2025-11-11 11:39:14 +0100

    Feature : ajout de la date de création d'une tache

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -6,6 +7,7 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Boolean, default=False)
-
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    
     def __repr__(self):
         return f'<Task {self.title}>'
