58266928abb41df30717440b7bf4aad51b3191ed
ARNAUDVictor <142483758+ARNAUDVictor@users.noreply.github.com>
2025-11-13 22:42:14 +0100

    Deploy: auto-initialisation des tables DB

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from models import db
 from config import Config
 from blueprints.tasks import tasks_bp
 
-#app creation
+# App creation
 app = Flask(__name__)
 
 # Load configuration from Config class
@@ -12,9 +12,17 @@ app.config.from_object(Config)
 # Initialize the database
 db.init_app(app)
 
+# Créer les tables automatiquement
+with app.app_context():
+    db.create_all()
+
 # Register blueprints
 app.register_blueprint(tasks_bp)
 
+# Route d'accueil (redirige vers /tasks/)
+@app.route('/')
+def index():
+    return redirect(url_for('tasks.home'))
 
 ########################Run the app########################
 if __name__ == '__main__':
